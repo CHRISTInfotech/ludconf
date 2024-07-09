d48459daa7f630570d69204b55bd08793d1db198
alwin-97 <alwinjosephkaduppil@gmail.com>
2024-07-09 19:50:47 +0530

    create conference work

--- conference/urls.py
+++ conference/urls.py
@@ -1,7 +1,11 @@
 from django.urls import path
 
-from conference.views import home
+from conference.views import home, ludlogin, admindashboard, adminconferencecreate, ludlogout
 
 urlpatterns = [
-    path('',home,name='home')
+    path('',home,name='home'),
+    path('login',ludlogin,name='ludlogin'),
+    path('logout',ludlogout,name='ludlogout'),
+    path('admin_dashboard',admindashboard,name='admin_dashboard'),
+    path('admin_conference_create',adminconferencecreate,name='admin_conference_create'),
 ]
--- conference/views.py
+++ conference/views.py
@@ -1,5 +1,41 @@
-from django.shortcuts import render
+from django.contrib import messages
+from django.contrib.auth import login, logout, authenticate
+from django.shortcuts import render, redirect
+
 
 # Create your views here.
 def home(request):
     return render(request,'conference/index.html')
+
+def ludlogin(request):
+    if request.method.lower() == 'post':
+        username = request.POST.get('email')
+        password = request.POST.get('password')
+        user = authenticate(username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return redirect('admin_dashboard')
+        else:
+            messages.error(request, 'Invalid credentials')
+            return redirect('ludlogin')
+    else:
+        return render(request,'userauth/login.html')
+
+
+def ludlogout(request):
+    if request.user.is_authenticated:
+        logout(request.user)
+    return redirect('home')
+
+def admindashboard(request):
+    if request.user.is_authenticated and request.user.is_superuser:
+        return render(request,'siteadmin/dashboard.html')
+    else:
+        return redirect('home')
+
+
+def adminconferencecreate(request):
+    if request.user.is_authenticated and request.user.is_superuser:
+        return render(request,'siteadmin/newconference.html')
+    else:
+        return redirect('home')
