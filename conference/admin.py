326695d95017839d631cec8d8fb6c86debf26dd0
alwin-97 <alwinjosephkaduppil@gmail.com>
2024-07-12 13:09:24 +0530

    adding admin registrations

--- conference/admin.py
+++ conference/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
 
+from conference.models import Conference, OTPRequest, UserDetails, ConferenceOrganisers, ConferenceDetails, \
+    ConferenceRegistration
+
 # Register your models here.
+admin.site.register(Conference)
+admin.site.register(OTPRequest)
+admin.site.register(UserDetails)
+admin.site.register(ConferenceOrganisers)
+admin.site.register(ConferenceDetails)
+admin.site.register(ConferenceRegistration)
